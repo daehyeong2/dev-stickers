34530dca39a045dd445e990cc55b6042bb34ac7d
daehyeong2 <daehyeong2010naver.com@gmail.com>
2024-01-23 14:18:53 +0900

    10.6

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import Layout from "../components/Layout";
+import Seo from "../components/Seo";
+
+export default function IndexPage() {
+  return (
+    <Layout title="Welcome to my DevStickers 🙌">
+      <div></div>
+    </Layout>
+  );
+}
+
+export const Head = () => <Seo title="Home" />;
